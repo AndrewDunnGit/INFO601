bf4bad431360d6596f0aea6332127d3ddd6ad2df
AndrewDunnGit <133712033+AndrewDunnGit@users.noreply.github.com>
2023-06-04 23:00:19 +1200

    Add files via upload
    Update 3/6/2022: 
    script.js and index.html updates to implement changes to the displied data formatting.
    fetchCharactersByLocation function added to script.js and corresponding button added to index.html

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Rick and morty API application
-testing linking to the rick and morty api using javascript.
+# api-test
+testing an api
+
+Update 3/6/2022: 
+script.js and index.html updates to implement changes to the displied data formatting.
+fetchCharactersByLocation function added to script.js and corresponding button added to index.html
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Rick and Morty API: https://rickandmortyapi.com/
-// Testing an API
 
 // Function that fetches our characters based on the name from the input field
 function fetchCharacters() {
@@ -13,13 +12,58 @@ function fetchCharacters() {
     });
 }
 
-// Function that formats the data that has been returned from our character fetch (and appends it to the DOM)
+// Function that fetches characters based on the location from the input field
+function fetchCharactersByLocation() {
+  const inputValue = document.getElementById('location-input').value;
+
+  // Using Fetch to get the API characters
+  fetch(`https://rickandmortyapi.com/api/location/?name=${inputValue}`)
+    .then(resp => resp.json())
+    .then(data => {
+      if (data.results.length > 0) {
+        const locationId = data.results[0].id;
+        fetch(`https://rickandmortyapi.com/api/location/${locationId}`)
+          .then(resp => resp.json())
+          .then(locationData => {
+            const characterIds = locationData.residents.map(characterUrl => {
+              const characterId = characterUrl.split('/').pop();
+              return characterId;
+            });
+            fetch(`https://rickandmortyapi.com/api/character/${characterIds.join(',')}`)
+              .then(resp => resp.json())
+              .then(charData => {
+                formatCharacters(charData);
+              });
+          });
+      } else {
+        formatCharacters([]);
+      }
+    });
+}
+
+
+
+
 function formatCharacters(characters) {
   const charDiv = document.getElementById('characters');
   charDiv.innerHTML = '';
 
-  // foreach to get all the characters
   characters.forEach(character => {
-    charDiv.innerHTML += `<div><img src="${character.image}"><h2>${character.name}</h2></div>`;
+    // Fetch the location data for the character
+    fetch(character.location.url)
+      .then(resp => resp.json())
+      .then(locationData => {
+        charDiv.innerHTML += `<div>
+          
+          <h2>${character.name}</h2>
+		  <img src="${character.image}">
+          <p>Status: ${character.status}</p>
+          <p>Species: ${character.species}</p>
+          <p>Gender: ${character.gender}</p>
+          <p>Location: ${locationData.name}</p>
+        </div>`;
+      });
   });
 }
+
+
